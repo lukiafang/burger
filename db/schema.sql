ec74be4adf0f202393b1bd96eeac46a44b977b9f
lukia <xhumen@msn.com>
2018-09-20 00:53:43 -0400

    setup orm.js sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,16 @@
-Inside your burger directory, create a folder named db.
-In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
+###schema
 
+CREATE DATABASE burgers_db.
+USE burgers_db;
 
-
-Create the burgers_db.
-Switch to or use the burgers_db.
-Create a burgers table with these fields:
-
-
-
-id: an auto incrementing int that serves as the primary key.
-
-burger_name: a string.
-
-devoured: a boolean.
-
-
-
-
-
-Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(45) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+)
+insert queries to populate the burgers table with at least three entries.
 Run the schema.sql and seeds.sql files into the mysql server from the command line
 Now you're going to run these SQL files.
 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Really Good Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Smoke');
+INSERT INTO burgers (burger_name) VALUES ('Big');
+INSERT INTO burgers (burger_name) VALUES ('Maryland');
+INSERT INTO burgers (burger_name) VALUES ('mc Biden');
+INSERT INTO burgers (burger_name) VALUES ('Owens Burger');
