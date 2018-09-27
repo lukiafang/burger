1aa293c40d35abeff2977deab0e030c26af8c4c8
lukia <xhumen@msn.com>
2018-09-27 02:21:22 -0400

    changed port to 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 // Sets up Express
 //==============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
