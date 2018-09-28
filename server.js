919b386dd8a2ceeaae67d9a25593c891fbb2d84e
lukia <xhumen@msn.com>
2018-09-28 00:27:28 -0400

    burger-C to c ontroller

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");// Set Handlebars.
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
